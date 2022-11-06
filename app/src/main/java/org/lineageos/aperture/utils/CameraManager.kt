LineageOS/android_packages_apps_Aperture

Aperture: Allow including logical cameras to the list of aux cameras
Change-Id: I8995e23829f77f7bca79918bb65c6d2909679927

app/src/main/java/org/lineageos/aperture/utils/CameraManager.kt
@@ -27,6 +27,9 @@ class CameraManager(activity: AppCompatActivity) {
     val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
 
     val ignoreCameraIds by lazy { activity.resources.getIntArray(R.array.ignoreCameraIds) }
+    val ignoreLogicalAuxCameras by lazy {
+        activity.resources.getBoolean(R.bool.ignoreLogicalAuxCameras)
+    }
 
     val cameras: Map<Int, Camera>
         get() = cameraProvider.availableCameraInfos.associate {
@@ -153,8 +156,10 @@ class CameraManager(activity: AppCompatActivity) {
             return listOf(mainCamera)
         }
 
-        // Get rid of logical cameras, we want single sensor cameras for now
-        val auxCameras = facingCameras.drop(1).filter { !it.isLogical }
+        // Get the list of aux cameras
+        val auxCameras = facingCameras
+            .drop(1)
+            .filter { !ignoreLogicalAuxCameras || !it.isLogical }
         // Setup zoom ratio for aux cameras
         for (camera in auxCameras) {
             camera.zoomRatio = camera.mm35FocalLengths[0] / mainCamera.mm35FocalLengths[0]

alecchangod-test-hub/packages_apps_Aperture

app/src/main/java/org/lineageos/aperture/utils/CameraManager.kt
@@ -29,6 +29,9 @@ class CameraManager(activity: AppCompatActivity) {
     val ignoredAuxCameraIds by lazy {
         activity.resources.getStringArray(R.array.config_ignoredAuxCameraIds)
     }
+    val ignoreLogicalAuxCameras by lazy {
+        activity.resources.getBoolean(R.bool.config_ignoreLogicalAuxCameras)
+    }
 
     val cameras: Map<Int, Camera>
         get() = cameraProvider.availableCameraInfos.associate {
@@ -155,8 +158,10 @@ class CameraManager(activity: AppCompatActivity) {
             return listOf(mainCamera)
         }
 
-        // Get rid of logical cameras, we want single sensor cameras for now
-        val auxCameras = facingCameras.drop(1).filter { !it.isLogical }
+        // Get the list of aux cameras
+        val auxCameras = facingCameras
+            .drop(1)
+            .filter { !ignoreLogicalAuxCameras || !it.isLogical }
         // Setup zoom ratio for aux cameras
         mainCamera.mm35FocalLengths?.getOrNull(0)?.let { mainCameraMm35FocalLength ->
             for (camera in auxCameras) {